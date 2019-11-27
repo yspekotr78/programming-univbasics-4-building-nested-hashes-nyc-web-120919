674240abbeb1c1e2eee0f28f474a26cc172b535c
Yury Spektor <yspektor78@gmail.com>
2019-11-27 15:40:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,8 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces=>4,
 	    :rent_in_dollars=>{
-	      :one_piece_owned=>25,:level2=>{},:level3=>{}, :level4=>{}
+	      :one_piece_owned=>25,
+	      :two_pieces_owned=>50,:level3=>{}, :level4=>{}
 	    },
 	    :names=>{
 	      :level1=>{},:level2=>{},:level3=>{}, :level4=>{}
