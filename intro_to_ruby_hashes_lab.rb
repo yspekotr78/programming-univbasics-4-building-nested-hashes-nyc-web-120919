963958400ba2a390d676ce59abd977c519bd82b6
Yury Spektor <yspektor78@gmail.com>
2019-11-27 15:33:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,9 @@ def monopoly_with_third_tier
   # complexity.
    {
 	  :railroads => {:pieces=>4,
-	                 :rent_in_dollars=>}
+	                 :rent_in_dollars=>{}
+	    
+	  }
 	}
 end
 
