feb9138f7945c0faba6ce350d59fd665626a0162
Yury Spektor <yspektor78@gmail.com>
2019-11-27 15:43:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
 	      :reading_railroad=>{},
 	      :pennsylvania_railroad=>{},
 	      :b_and_o_railroad=>{},
-	      :level4=>{}
+	      :shortline_railroad=>{}
 	    }
 	  }
 	}
