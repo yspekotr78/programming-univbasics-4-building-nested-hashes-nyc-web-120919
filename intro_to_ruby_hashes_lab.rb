0044d0021d8b6295d92ae4ba8cf4a669b8abc40f
Yury Spektor <yspektor78@gmail.com>
2019-11-27 15:29:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
    {
-	  :railroads => {:pieces=>4}#,{:rent_in_dollars}
+	  :railroads => {:pieces=>4},{:rent_in_dollars}
 	}
 end
 
