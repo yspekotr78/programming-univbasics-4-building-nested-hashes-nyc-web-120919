0c831730e9829553165e4d0fe8b4cea5707fbdec
Yury Spektor <yspektor78@gmail.com>
2019-11-27 15:18:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,16 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	{
-	  
+	  :north_america => {
+    "Canada" => {
+      :capital => "Ottawa",
+      :capital_climate => "Kőppen Dfb"
+    },
+    "USA" => {
+      :capital => "Washington D.C.",
+      :capital_climate => "Kőppen Cfa"
+    }
+  }
 	}
 end
 
