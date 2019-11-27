c05ebc7940648ddc9ed040a8768caef579a1541a
Yury Spektor <yspektor78@gmail.com>
2019-11-27 15:42:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
 	    :names=>{
 	      :reading_railroad=>{},
 	      :pennsylvania_railroad=>{},
-	      :level3=>{},
+	      :b_and_o_railroad=>{},
 	      :level4=>{}
 	    }
 	  }
